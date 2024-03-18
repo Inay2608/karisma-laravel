670cd00fcf734320a4f1dc4fe30f50cbaedec8c1
inay41268 <inay41268@gmail.com>
2024-03-18 21:58:45 +0700

    first commit

---
+++ resources/views/introduction.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Introduction</title>
+</head>
+<body>
+    <h1 style="text-align: center">Hello World!</h1>
+    <p style="text-align: center">Saya Inayati Sa'adah. Saya Sedang Belajar Laravel di <b>Karisma Academy<b></p>
+</body>
+</html>
